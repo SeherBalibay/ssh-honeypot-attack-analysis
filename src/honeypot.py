d3b64288ac42a31ef55909d22183c44d676b0a6c
SeherBalibay <seherblby@gmail.com>
2026-06-11 14:29:03 +0300

    Added username password logging

--- src/honeypot.py
+++ src/honeypot.py
@@ -1,9 +1,11 @@
 import socket
+from datetime import datetime
 
 HOST = "0.0.0.0"
-PORT = 2222
+PORT = 5050
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
 server.bind((HOST, PORT))
 server.listen(5)
@@ -13,8 +15,26 @@ print(f"[+] SSH Honeypot listening on port {PORT}")
 while True:
     client, address = server.accept()
 
-    print(f"[ATTACK] Connection received from {address[0]}")
+    ip = address[0]
+    timestamp = datetime.now()
 
-    client.send(b"Fake SSH Service\r\n")
+    print(f"\n[ATTACK] Connection from {ip}")
+    print(f"[TIME] {timestamp}")
+
+    client.send(b"Username: ")
+
+    username = client.recv(1024).decode().strip()
+
+    client.send(b"Password: ")
+
+    password = client.recv(1024).decode().strip()
+
+    print(f"[USERNAME] {username}")
+    print(f"[PASSWORD] {password}")
+
+    with open("attack_logs.txt", "a", encoding="utf-8") as log:
+        log.write(
+            f"{timestamp} | IP:{ip} | USER:{username} | PASS:{password}\n"
+        )
 
     client.close()
